9a23d2ef44cba86139f3ea4333b9b9455ba1b18b
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-08-17 19:07:31 +0200

    Step 4
    Integración de la Lógica de Negocio, cargar módulo de Usuarios, definir Servicios y .html para el Contacto.

--- app/src/app.js
+++ app/src/app.js
@@ -4,7 +4,10 @@ import 'angular-animate';
 import 'angular-aria';
 import 'angular-material';
 
-export default angular.module('app', ['ngMaterial'])
+import AppController from './AppController';
+import Users from './users/Users';
+
+export default angular.module('app', ['ngMaterial', Users.name])
     .run(() => {
         console.log('Running "app" module...');
     });
